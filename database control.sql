08e7e43d35c9fdbd00f2dbc31063fc45fed0c2d7
kqlqk <kononchuk99999@gmail.com>
2024-01-03 12:54:39 +0100

    create basic database entities
    * was created 'customer' db and 'events' db

---
+++ database control.sql
@@ -0,0 +1,20 @@
+create table customer
+(
+    id   bigint generated always as identity primary key,
+    name varchar(50) not null,
+    age  int not null
+);
+
+create table events
+(
+    id   bigint generated always as identity primary key,
+    name varchar(100) not null,
+    description text not null ,
+    place varchar(100) not null ,
+    coordinates point,
+    event_time timestamptz not null,
+    creator_id bigint not null,
+
+    foreign key (creator_id) references customer(id)
+    on delete cascade on update cascade
+);
