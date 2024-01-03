7e92e3055104124eea9d81ff7926026b5d38cc8e
kqlqk <kononchuk99999@gmail.com>
2024-01-03 13:14:21 +0100

    add lombok
    * was added lombok dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
     implementation("org.postgresql:postgresql:42.7.1")
+    compileOnly("org.projectlombok:lombok:1.18.30")
 }
 
 java {
