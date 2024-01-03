9aa817d08eaea1a67313c6e17d1bf9431b073db5
kqlqk <kononchuk99999@gmail.com>
2024-01-03 13:14:00 +0100

    add connection to database
    * was added all data to connect to database in application.properties
    
    * was added all required dependencies in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,9 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.1")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf:3.2.1")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.2.1")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.2.1")
+    implementation("org.postgresql:postgresql:42.7.1")
 }
 
 java {
