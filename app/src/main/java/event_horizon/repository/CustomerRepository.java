e1e48e8b1b23a4dfd988db512ef1a06e8d098d12
kqlqk <kononchuk99999@gmail.com>
2024-01-07 13:00:53 +0100

    add customer repository
    * was added CustomerRepository.java

---
+++ app/src/main/java/event_horizon/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package event_horizon.repository;
+
+import event_horizon.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
