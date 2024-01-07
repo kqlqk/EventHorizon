c3907a68bc6f8a98550bcbb22a3e6c507a9c07cf
kqlqk <kononchuk99999@gmail.com>
2024-01-07 13:27:51 +0100

    add customer controller
    * was added CustomerController.java and mapping GET '/user/{id} with CustomerPage.html'
    
    * was added unit tests and it

---
+++ app/src/test/java/event_horizon/controller/CustomerControllerIT.java
@@ -0,0 +1,32 @@
+package event_horizon.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@ActiveProfiles("test")
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+@AutoConfigureMockMvc
+public class CustomerControllerIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getCustomerPage_ReturnsCustomerPage() throws Exception {
+        mockMvc.perform(get("/user/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("customer/CustomerPage"))
+                .andExpect(model().attributeExists("customer"));
+    }
+}
